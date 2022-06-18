13cb49aab00c9add80829844ba321094ae287184
Subhi-c <csubhiksha23@gmail.com>
2022-06-17 18:29:21 -0700

    Day 06 completed

--- Day 06 - Scroll Animation/script.js
+++ Day 06 - Scroll Animation/script.js
@@ -1,6 +1,16 @@
 const boxes = document.querySelectorAll('.box')
-window.addEventListener('click',active)
+window.addEventListener('scroll',active)
 active()
 function active(){
-  boxes.forEach(box)
+  const bottom = window.innerHeight / 5 * 4
+
+  boxes.forEach(box => {
+    const top = box.getBoundingClientRect().top
+    if(top < bottom){
+      box.classList.add('show')
+    }
+    else{
+      box.classList.remove('show')
+    }
+  })
 }
